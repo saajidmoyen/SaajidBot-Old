a81824c570930590c7f0a518cff03189f785ae4a
bangladude <saajidmoyen@gmail.com>
2012-11-22 16:10:12 -0500

    README Added

--- TMain.java
+++ TMain.java
@@ -1,17 +1,6 @@
 // Version 0.1 of PennQBBot by Saajid Moyen, 11/22/2012
 //
-// #scobowl bot © 2006 by Alejandro López-Lago
-// Bot started Feb. 26 2006
-/*
-Class descriptions:
-
-tmain: the driver class.  Creates the bot and connects it to a server
-tbot: the main code of the bot.  It runs the games, asking questions and creating players/teams.
-Player: It stores information about a player in the game.
-Team: It stores information about the two teams in the game.
-Question: Stores a question from the question set.
-*/
-//import org.jibble.pircbot.*;
+// Bot started Feb. 26 2006, by Alejandro López-Lago
 
 public class TMain{
 
